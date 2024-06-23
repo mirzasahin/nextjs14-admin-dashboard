5b80fa3b378eea418a431168c66fbe7b6d6a0c29
Mirza Sahin <salihmirza123@gmail.com>
2024-06-23 14:59:48 +0300

    Add navbar section

--- src/ui/dashboard/sidebar/sidebar.jsx
+++ src/ui/dashboard/sidebar/sidebar.jsx
@@ -98,6 +98,10 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
 
     </div>
   )
