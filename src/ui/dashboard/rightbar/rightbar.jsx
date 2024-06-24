d9dc50b12a6112f051fe9ea09b2917b4cd5e030e
Mirza Sahin <salihmirza123@gmail.com>
2024-06-24 20:32:03 +0300

    Implement rightbar section & footer

--- src/ui/dashboard/rightbar/rightbar.jsx
+++ src/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import styles from "./rightbar.module.css";
+import Image from "next/image";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
 
 const Rightbar = () => {
   return (
-    <div>Rightbar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quos
+            perspiciatis. Nihil quisquam nobis iusto facere.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
 
-export default Rightbar
+      <div className={styles.item}>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions are available, partial pre-rendering is coming
+            up!
+          </h3>
+          <span className={styles.subtitle}>Boost your productivity</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quos
+            perspiciatis. Nihil quisquam nobis iusto facere.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;
