4ebf8875e9f2768e3c34178a8044f5cf48091646
Mirza Sahin <salihmirza123@gmail.com>
2024-06-27 17:07:36 +0300

    Configure MongoDB & models structure

---
+++ src/app/lib/models.js
@@ -0,0 +1,79 @@
+import mongoose from 'mongoose'
+import {
+    Schema
+} from 'mongoose'
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+        min: 3,
+        max: 20,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        unique: true,
+    },
+    img: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    address: {
+        type: String,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+
+}, {
+    timestamps: true
+})
+
+const productSchema = new Schema({
+    title: {
+        type: number,
+        unique: true,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+        min: 0
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    img: {
+        type: String,
+    },
+    color: {
+        type: String,
+    },
+    size: {
+        type: String,
+    },
+}, {
+    timestamps: true
+})
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema)
+export const Product = mongoose.models.Product || mongoose.model("Product", productSchema)
